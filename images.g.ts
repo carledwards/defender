917dcef6dad41aecb249bed7e859d4548bd90d54
Carl Edwards <ecarl42@gmail.com>
2022-01-16 09:31:59 -0800

    Spaceship up/down movement, left/right changing the sprite's direction

---
+++ images.g.ts
@@ -0,0 +1,42 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "6g*Sl9G{([Bm9/`EtT0g":
+            case "Spaceship Left":return img`
+. . . . 8 . . . . . . . . . 
+. . . 8 . . . . . . . . . . 
+. . 8 . . . . . . . . . . . 
+. 8 . . . . . . . . . . . . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. 8 . . . . . . . . . . . . 
+. . 8 . . . . . . . . . . . 
+. . . 8 . . . . . . . . . . 
+. . . . 8 . . . . . . . . . 
+`;
+            case "image1":
+            case "Spaceship Right":return img`
+. . . . . . . . . 8 . . . . 
+. . . . . . . . . . 8 . . . 
+. . . . . . . . . . . 8 . . 
+. . . . . . . . . . . . 8 . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . . . . . . . . . . . 8 . 
+. . . . . . . . . . . 8 . . 
+. . . . . . . . . . 8 . . . 
+. . . . . . . . . 8 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,59 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveShipUp()
+})
+function moveShipUp () {
+    if (isGameStarted == true) {
+        spaceship.y += -3
+        if (spaceship.y < 10) {
+            spaceship.y = 10
+        }
+    }
+}
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveShipUp()
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (isGameStarted == true) {
+        setDirection(0)
+    }
+})
+function moveShipDown () {
+    if (isGameStarted == true) {
+        spaceship.y += 3
+        if (spaceship.y > (scene.screenHeight() - 10)) {
+            spaceship.y = scene.screenHeight() - 10
+        }
+    }
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (isGameStarted == true) {
+        setDirection(1)
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveShipDown()
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveShipDown()
+})
+function setup () {
+    direction = 1
+    spaceship = sprites.create(assets.image`Spaceship Right`, SpriteKind.Player)
+    setDirection(direction)
+    isGameStarted = true
+}
+function setDirection (value: number) {
+    if (value != direction) {
+        direction = value
+        if (value == 0) {
+            spaceship.setImage(assets.image`Spaceship Left`)
+        } else {
+            spaceship.setImage(assets.image`Spaceship Right`)
+        }
+    }
+}
+let direction = 0
+let spaceship: Sprite = null
+let isGameStarted = false
+isGameStarted = false
+setup()
