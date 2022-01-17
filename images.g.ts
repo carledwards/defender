36ffa8c7f3b3d2a09f7eefff873fc209c39a8181
Carl Edwards <ecarl42@gmail.com>
2022-01-16 19:44:39 -0800

    Spaceship moves left/right and shoots projectile

--- images.g.ts
+++ images.g.ts
@@ -3,29 +3,31 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "6g*Sl9G{([Bm9/`EtT0g":
-            case "Spaceship Left":return img`
-. . . . 8 . . . . . . . . . 
-. . . 8 . . . . . . . . . . 
-. . 8 . . . . . . . . . . . 
-. 8 . . . . . . . . . . . . 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-. 8 . . . . . . . . . . . . 
-. . 8 . . . . . . . . . . . 
-. . . 8 . . . . . . . . . . 
-. . . . 8 . . . . . . . . . 
-`;
             case "image1":
             case "Spaceship Right":return img`
-. . . . . . . . . 8 . . . . 
-. . . . . . . . . . 8 . . . 
-. . . . . . . . . . . 8 . . 
-. . . . . . . . . . . . 8 . 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-. . . . . . . . . . . . 8 . 
-. . . . . . . . . . . 8 . . 
-. . . . . . . . . . 8 . . . 
-. . . . . . . . . 8 . . . . 
+. . d d . . . . . . . . . . . 
+. d d d d . . . . . . . . . . 
+2 d d d d d . . . . . . . . . 
+. d a a d d d d d 8 8 5 . . . 
+2 d a a a a d d d d d d d 1 7 
+. . a a a d 1 7 . . . . . . . 
+`;
+            case "6g*Sl9G{([Bm9/`EtT0g":
+            case "Spaceship Left":return img`
+. . . . . . . . . . . d d . . 
+. . . . . . . . . . d d d d . 
+. . . . . . . . . d d d d d 2 
+. 8 . 5 8 8 d d d d d a a d . 
+7 1 d d d d d d d a a a a d 2 
+. . . . . . . 7 1 d a a a . . 
+`;
+            case "image2":
+            case "missleRight":return img`
+2 2 2 2 3 2 3 2 
+`;
+            case "image3":
+            case "missleLeft":return img`
+2 3 2 3 2 2 2 2 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -3,11 +3,9 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function moveShipUp () {
     if (isGameStarted == true) {
-        if (spaceship.y <= 10) {
-            spaceship.y = 10
+        if (spaceship.vy > 0) {
             spaceship.vy = 0
-        }
-        else {
+        } else {
             spaceship.vy = -40
             spaceship.y += -2
         }
@@ -16,46 +14,49 @@ function moveShipUp () {
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
     moveShipUp()
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (direction == 0) {
+        projectile = sprites.createProjectileFromSprite(assets.image`missleLeft`, spaceship, -100, 0)
+        projectile.y += 2
+        projectile.x += -3
+    } else {
+        projectile = sprites.createProjectileFromSprite(assets.image`missleRight`, spaceship, 100, 0)
+        projectile.y += 2
+        projectile.x += 3
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveShipRight()
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveShipLeft()
+})
+function moveShipLeft () {
     if (isGameStarted == true) {
         setDirection(0)
-    }
-})
-function moveShipDown () {
-    if (isGameStarted == true) {
-        spaceship.vy = 40
-        spaceship.y += 2
-        // if (spaceship.y > (scene.screenHeight() - 10)) {
-        //     spaceship.y = scene.screenHeight() - 10
-        // }
+        moveDirection()
     }
 }
-
-game.onUpdate(function() {
+function moveShipDown () {
     if (isGameStarted == true) {
-        if (spaceship.y <= 10) {
-            spaceship.y = 10
+        if (spaceship.vy < 0) {
             spaceship.vy = 0
-        }
-        else if (spaceship.y >= (scene.screenHeight() - 10)) {
-            spaceship.y = scene.screenHeight() - 10
-            spaceship.vy = 0
-        }
-
-        if (spaceship.vy > 0) {
-            spaceship.vy += -1
-        }
-        else if (spaceship.vy < 0) {
-            spaceship.vy += 1
+        } else {
+            spaceship.vy = 40
+            spaceship.y += 2
         }
     }
-})
-
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (isGameStarted == true) {
-        setDirection(1)
-    }
+    moveShipRight()
 })
+function moveDirection () {
+    if (direction == 0) {
+        velocity = 60
+    } else {
+        velocity = -60
+    }
+}
 controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
     moveShipDown()
 })
@@ -63,11 +64,17 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     moveShipDown()
 })
 function setup () {
-    controller.configureRepeatEventDefaults(100, 20)
+    controller.configureRepeatEventDefaults(100, 25)
     direction = 1
     spaceship = sprites.create(assets.image`Spaceship Right`, SpriteKind.Player)
     setDirection(direction)
     isGameStarted = true
+}
+function moveShipRight () {
+    if (isGameStarted == true) {
+        setDirection(1)
+        moveDirection()
+    }
 }
 function setDirection (value: number) {
     if (value != direction) {
@@ -79,8 +86,50 @@ function setDirection (value: number) {
         }
     }
 }
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveShipLeft()
+})
+let velocity = 0
+let projectile: Sprite = null
 let direction = 0
 let spaceship: Sprite = null
 let isGameStarted = false
+let sideMargin = 40
 isGameStarted = false
 setup()
+game.onUpdate(function () {
+    if (isGameStarted == true) {
+        if (direction == 0) {
+            if (spaceship.x < scene.screenWidth() - sideMargin) {
+                spaceship.vx = velocity
+            } else {
+                spaceship.x = scene.screenWidth() - sideMargin
+                spaceship.vx = 0
+            }
+        } else {
+            if (spaceship.x > sideMargin) {
+                spaceship.vx = velocity
+            } else {
+                spaceship.x = sideMargin
+                spaceship.vx = 0
+            }
+        }
+        if (velocity > 0) {
+            velocity += -1
+        } else if (velocity < 0) {
+            velocity += 1
+        }
+        if (spaceship.y <= 10) {
+            spaceship.y = 10
+            spaceship.vy = 0
+        } else if (spaceship.y >= scene.screenHeight() - 10) {
+            spaceship.y = scene.screenHeight() - 10
+            spaceship.vy = 0
+        }
+        if (spaceship.vy > 0) {
+            spaceship.vy += -1
+        } else if (spaceship.vy < 0) {
+            spaceship.vy += 1
+        }
+    }
+})
